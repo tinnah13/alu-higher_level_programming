bc82a7e6e07adfb06a4db9d1f94724e9e823e71a
tinnah13 <a.umuhoza2@alustudent.com>
2026-03-18 06:37:52 -0700

    done with file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""rread file"""
+
+
+def read_file(filename=""):
+    """read file"""
+    with open(filename) as f:
+        line = f.read()
+        print(line, end="")#!/usr/bin/python3
+"""rread file"""
+
+
+def read_file(filename=""):
+    """read file"""
+    with open(filename) as f:
+        line = f.read()
+        print(line, end="")
